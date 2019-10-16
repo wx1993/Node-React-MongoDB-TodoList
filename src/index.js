72040b5ec0ffb3befc5eb6698cc43c8ee9912312
Sander <sendongliang@gmail.com>
2019-10-16 16:18:22 +1300

    # 1 add logo.jpg search main page change.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import Router from './router/index.js'
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import store from './store/index.js';
-import {updateStaff,updateDept} from './global/initBaseData.js';
+import {search,updateDept} from './global/initBaseData.js';
 if (module.hot) {
   // 模块自己就接收更新
   module.hot.accept();
 }
 React.Component.prototype.$http = axios;
-// 取得部门数据和员工的数据
-updateStaff.bind(React.Component.prototype)();
+// updateStaff.bind(React.Component.prototype)();
+search.bind(React.Component.prototype)({});
 updateDept.bind(React.Component.prototype)();
 ReactDOM.render(<Provider store={store}>
     <Router />
